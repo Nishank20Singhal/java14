cc54708c61d1702e1ad101acfe88a6c605844139
Nishank20Singhal <nishank20singhal@gmail.com>
2021-02-26 18:49:33 +0530

    first commit

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,4 +1,4 @@
 insert into authorities (authority) values ('ROLE_USER'), ('ROLE_ADMIN');
 
-insert into posts (name, description) values ('What is this forum about?', 'What is this forum about. Description of the forum .... '),
-('Forum rules', 'Forum rules rule 1 - description of the rule, rule 2 - more description ...');
+insert into posts (name, description) values ('О чем этот форум?', 'О чем этот форум. Описание форума....'),
+('Правила форума', 'Правила форумаю правило 1 - описнаие правила, правило 2 - еще описание...');
